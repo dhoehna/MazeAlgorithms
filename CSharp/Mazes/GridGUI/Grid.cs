7d3d7032be043259ec4bc7030d3a7f91ca8bb7cf
dhoehna <dhoehna@yahoo.com>
2017-04-25 19:03:38 -0700

    Adding code to make a grid

---
+++ CSharp/Mazes/GridGUI/Grid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridGUI
+{
+    public class Grid
+    {
+        private Room[][] rooms;
+
+        public Grid(int numberOfRows, int numberOfColumns)
+        {
+            rooms = new Room[numberOfRows][];
+
+            for(int x = 0; x < numberOfRows; x++)
+            {
+                rooms[x] = new Room[numberOfColumns];
+            }
+        }
+    }
+}
