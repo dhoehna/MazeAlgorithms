07ec600ff656d4d7ab095d89b75098a0d5ad8191
dhoehna <dhoehna@yahoo.com>
2017-04-25 21:29:13 -0700

    Adding a room helper

---
+++ CSharp/Mazes/GridGUI/RoomHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static GridGUI.Grid;
+
+namespace GridGUI
+{
+    public sealed class RoomHelper
+    {
+
+        public static GridPosition GetNeighborLocation(Room room, Direction direction)
+        {
+            if(direction == Direction.NORTH)
+            {
+                return new GridPosition(room.row - 1, room.column);
+            }
+            else if (direction == Direction.SOUTH)
+            {
+                return new GridPosition(room.row + 1, room.column);
+            }
+            else if (direction == Direction.EAST)
+            {
+                return new GridPosition(room.row, room.column - 1);
+            }
+            else if (direction == Direction.WEST)
+            {
+                return new GridPosition(room.row, room.column + 1);
+            }
+            else
+            {
+                throw new ArgumentException($"{direction} is not a valid direction.");
+            }
+        }
+    }
+}
