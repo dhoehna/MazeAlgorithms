b0ec353255972f193966a89b96592fdc83188eef
Darren Hoehna <dhoehna@yahoo.com>
2017-04-27 16:02:07 -0700

    Adding other classes and projects I think I'll need

--- CSharp/Mazes/Grid/Grid.cs
+++ CSharp/Mazes/Grid/Grid.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Grid;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace GridGUI
 {
-    public sealed class Grid
+    public sealed class Grid : IGrid
     {
         public enum Direction
         {
---
+++ CSharp/Mazes/Grid/IGrid.cs
@@ -0,0 +1,13 @@
+﻿using GridGUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static GridGUI.Grid;
+
+namespace Grid
+{
+    interface IGrid
+    {
+        void Connect(Room roomToConnectWith, Direction directionToConnect);
+    }
+}
