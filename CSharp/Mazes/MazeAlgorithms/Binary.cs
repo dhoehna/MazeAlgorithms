5c1d531ff40d1095efc183dfefee1a81286ca638
Darren Hoehna <dhoehna@yahoo.com>
2017-05-03 14:05:24 -0700

    Starting the BinaryAlgortihm.

--- CSharp/Mazes/MazeAlgorithms/Binary.cs
+++ CSharp/Mazes/MazeAlgorithms/Binary.cs
@@ -1,15 +1,21 @@
 ﻿
 using System;
 using Grid;
+using System.Collections.Generic;
+using static Grid.Grid;
+
 namespace MazeAlgorithms
 {
     public class Binary :IMazeAlgorithm
     {
         public void TurnGridIntoMaze(IGrid gridToManipulate)
         {
-            foreach(Room room in gridToManipulate.GetRoomsSequantially())
+            List<Room> rooms = gridToManipulate.GetRooms();
+            int rows = gridToManipulate.GetRows();
+            int columns = gridToManipulate.GetColumns();
+            foreach(Room room in rooms)
             {
-
+                List<Direction> edges = RoomHelper.GetBoundriesRoomIsOn(room, rows, columns);
             }
         }
     }
