84fd32e71d93119cb4db5fa5bcf84e71036c933b
Darren Hoehna <dhoehna@yahoo.com>
2017-05-10 15:39:38 -0700

    Alright, got a grad to show up.

--- CSharp/Mazes/Mazes/Program.cs
+++ CSharp/Mazes/Mazes/Program.cs
@@ -15,16 +15,33 @@ namespace Mazes
         {
             /*
              * This is a 5 by 5 grid.
-             * Each cell is 10 pixles
+             * Each cell is 100 pixles
              * so that means the image has to be 10 * 5, or 500 pixles wide and long
              */
             Bitmap b = new Bitmap(500, 500);
 
             Graphics g = Graphics.FromImage(b);
 
-            g.Clear(Color.Green);
+
+            Pen blackPen = new Pen(Color.Black, 3);
+
+            //Border
+            g.DrawRectangle(blackPen, 0, 0, 500, 500);
+
+            g.DrawLine(blackPen, 100, 0, 100, 500);
+            g.DrawLine(blackPen, 200, 0, 200, 500);
+            g.DrawLine(blackPen, 300, 0, 300, 500);
+            g.DrawLine(blackPen, 400, 0, 400, 500);
+
+            g.DrawLine(blackPen, 0, 100, 500, 100);
+            g.DrawLine(blackPen, 0, 200, 500, 200);
+            g.DrawLine(blackPen, 0, 300, 500, 300);
+            g.DrawLine(blackPen, 0, 400, 500, 400);
+
 
             b.Save("Hello.png");
+
+            blackPen.Dispose();
 
             g.Dispose();
 
