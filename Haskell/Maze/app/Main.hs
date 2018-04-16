02c42898eb895b18cea33e97e5e3bfd2e69e26c4
dhoehna <dhoehna@yahoo.com>
2018-04-15 20:19:33 -0700

    Adding neightbors

--- Haskell/Maze/app/Main.hs
+++ Haskell/Maze/app/Main.hs
@@ -2,5 +2,24 @@ module Main where
 
 import Lib
 
+
+--grid
+--cell
+--neighbors NORTH, SOUTH, EAST, WEST
+
+data HasNorthNeighbor = HasNorthNeighbor
+data HasSouthNeighbor = HasSouthNeighbor
+data HasEastNeighbor = HasEastNeighbor
+data HasWestNeighbor = HasWestNeighbor
+
+--Cell :: HasNorthNeighbor -> HasSouthNeighbor -> HasEastNeighbor -> HasWestNeighbor -> Cell
+data Cell = Cell HasNorthNeighbor HasSouthNeighbor HasEastNeighbor HasWestNeighbor
+
+
 main :: IO ()
 main = someFunc
+
+
+
+
+
