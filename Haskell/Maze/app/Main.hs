800ba211b444ab950bc27c16746cfd6da65e3af4
dhoehna <dhoehna@yahoo.com>
2019-02-02 16:31:56 -0800

    Moving code out of Main

--- Haskell/Maze/app/Main.hs
+++ Haskell/Maze/app/Main.hs
@@ -1,20 +1,34 @@
 module Main where
+import Cell
 
-import Lib
-  
-newtype HasNorthNeighbor = HasNorthNeighbor Bool
-newtype HasSouthNeighbor = HasSouthNeighbor Bool
-newtype HasEastNeighbor = HasEastNeighbor Bool
-newtype HasWestNeighbor = HasWestNeighbor Bool
-  
-data Cell = MakeCell
-   {
-       HasNorthNeighbor
-       ,HasSouthNeighbor
-       ,HasEastNeighbor
-       ,HasWestNeighbor
-   }
+type Maze = [Cell]
+
+maxWidth :: Int
+maxWidth = 5
+
+maxHeigth :: Int
+maxHeigth = 5
+
+numberOfCells :: Int
+numberOfCells = maxWidth * maxHeigth
+
+canCellMakeNorthNeighbor :: Int -> Bool
+canCellMakeNorthNeighbor xPositionOfCell = xPositionOfCell > 0
+
+canCellMakeEastNeighbor :: Int -> Bool
+canCellMakeEastNeighbor yPositionOfCell = yPositionOfCell < 4
+
+getCellFromIndex :: Maze -> Int -> Int -> Int -> Cell
+getCellFromIndex maze width heigth index = maze !! (width + (maxWidth * heigth))
+
+fillInMaze :: [Bool -> Bool -> Bool -> Bool -> Int -> Int -> Cell]
+fillInMaze = [cellConstructor | cell <- [0..numberOfCells]]
+
+getRandomCharacter :: Char
+getRandomCharacter = 'a'
 
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn "Hello"
+    --putStrLn fillInMaze
